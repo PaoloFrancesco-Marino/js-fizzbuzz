23d2dfd202e8809549c0e95873ea2a92f6fb599e
Paolo Francesco Marino <paolo.francesco.marino@gmail.com>
2020-04-01 16:28:21 +0200

    Simplify JS

--- script/main.js
+++ script/main.js
@@ -2,24 +2,23 @@
  * Fizz Buzz
  */
 
-//  Scrivi un programma che stampi i numeri da 1 a 100, ma:
-
-for (var i = 0; i <= 100; i++) {
+//  Scrivi un programma che stampi i numeri da 1 a 100
+for (var i= 0; i <= 100; i++) {
     
-    if ((i % 3 == 0) && (i % 5 !=0)) {
-        // per i multipli di 3 stampi “Fizz” al posto del numero
-        console.log(i + ' fizz');
-    } else if ((i % 5 == 0) && (i % 3 !=0)) {
-        // per i multipli di 5 stampi Buzz. 
-        console.log(i + ' buzz');
-    } else if ((i % 3 == 0) && (i % 5 == 0)) {
+    if ((i % 3 == 0) && (i % 5 == 0)) {
         // per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
-        console.log(i + 'FizzBuzz');
+        console.log(i + ' FizzBuzz');    
+    } else if (i % 3 == 0) {
+        // per i multipli di 3 stampi “Fizz” al posto del numero
+        console.log(i + ' Fizz');
+    } else if (i % 5 == 0) {
+        // per i multipli di 5 stampi Buzz. 
+        console.log(i + ' Buzz');
     } else {
+        // numeri restanti
         console.log(i);   
     }
 }
-
 
 
 
